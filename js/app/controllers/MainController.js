265094e47dd036846b9c6748b54be144ababe481
Tyler Machen <tylermachen@gmail.com>
2016-06-27 20:35:27 -0400

    done

---
+++ js/app/controllers/MainController.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('MainController', function($scope) {
+    $scope.name = 'Tyler Machen';
+    $scope.email = 'tylermachen@gmail.com';
+    $scope.phone = '123-456-7891';
+});
